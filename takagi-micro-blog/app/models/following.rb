67ae090367db2d54d06ba2fbdcb93fba32c24575
Go Takagi <takagi@shimastripe.com>
2016-04-11 12:12:42 +0900

    Set up associations

--- takagi-micro-blog/app/models/following.rb
+++ takagi-micro-blog/app/models/following.rb
@@ -1,4 +1,4 @@
 class Following < ApplicationRecord
   belongs_to :user
-  belongs_to :followed_user
+  belongs_to :followed_user, class_name: "User"
 end
--- takagi-micro-blog/app/models/user.rb
+++ takagi-micro-blog/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :micro_blogs
+  has_many :followings
+  has_many :followers, through: :followings, source: :followed_user
 end
